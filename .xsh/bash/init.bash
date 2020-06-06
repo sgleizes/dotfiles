d19cbe78f737f6990ea97acea91c5248cef3bf6a
sgleizes <stephane.gleizes@gmail.com>
2020-06-06 14:22:35 +0200

    Add bash configuration

---
+++ .xsh/bash/init.bash
@@ -0,0 +1,9 @@
+#
+# This file is sourced automatically by xsh if the current shell is `bash`.
+# It should merely register the manager(s) and modules to be loaded by
+# each runcom (env, login, interactive, logout).
+# The order in which the modules are registered defines the order in which
+# they will be loaded. Try `xsh help` for more information.
+#
+
+xsh module core -s bash:posix
---
+++ .xsh/bash/module/core/@interactive.bash
@@ -0,0 +1,26 @@
+#
+# Core configuration module for bash.
+#
+
+xsh load core -s posix
+
+# Directory management
+
+shopt -s autocd      # automatically change to directory if the command is a directory
+shopt -s cdable_vars # allow automatic parameter expansion in cd context
+shopt -s cdspell     # allow automatic spelling correction in cd context
+shopt -s dirspell    # allow automatic spelling correction in pathname completion context
+
+# History management
+
+HISTCONTROL=ignoredups # ignore duplicate history entries
+shopt -s histverify    # do not execute immediately upon history expansion
+
+# Job management
+
+shopt -s checkjobs # report running/suspended jobs on shell exit
+shopt -s huponexit # kill jobs with SIGHUP on shell exit
+
+# Pattern matching
+
+shopt -s extglob # enable pattern lists separated with '|' and related operators
