9fce3d740f2999747a2ad8f970781f85fdbb0cef
sgleizes <stephane.gleizes@gmail.com>
2020-06-06 15:00:16 +0200

    Add history-substring-search module for zsh

---
+++ .xsh/zsh/module/history-substring-search/@interactive.zsh
@@ -0,0 +1,39 @@
+#
+# History substring search configuration module for zsh.
+#
+# NOTE: Similarly to syntax-highlighting, this module wraps ZLE widgets and
+# should be loaded after all widgets have been defined.
+# For best results, syntax-highlighting should be loaded before so that
+# highlighting is updated when the plugin updates the editor buffer.
+#
+
+# Abort if requirements are not met.
+if (( ! $+functions[zinit] )) {
+  return 1
+}
+
+# Configure zsh-history-substring-search.
+# This is provided as a function since the plugin is loaded asynchronously
+# and does not support overriding the defaults.
+function _configure_history_substring_search {
+  # Half case-sensitive: lower matches upper, upper does not match lower.
+  HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS=l
+
+  # Use less flashy colors.
+  if (( $terminfo[colors] >= 256 )) {
+    HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=62,fg=white,bold'
+    HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=88,fg=white,bold'
+  } elif (( $terminfo[colors] >= 8 )) {
+    HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=blue,fg=white,bold'
+    HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=red,fg=white,bold'
+  }
+
+  # Rebind up/down keys to use the loaded widgets.
+  bindkey "$keys[Up]" history-substring-search-up
+  bindkey "$keys[Down]" history-substring-search-down
+
+  unfunction _configure_history_substring_search
+}
+
+zinit ice wait lucid depth=1 atload'_configure_history_substring_search'
+zinit light zsh-users/zsh-history-substring-search
