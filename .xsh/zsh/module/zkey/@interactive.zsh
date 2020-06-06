7a97cc4ab48579d35d4d721c31b48c0e2126d610
sgleizes <stephane.gleizes@gmail.com>
2020-06-06 15:02:18 +0200

    Add zopt/zkey modules for zsh

---
+++ .xsh/zsh/module/zkey/@interactive.zsh
@@ -0,0 +1,67 @@
+#
+# Keymap monitoring module for zsh.
+#
+# NOTE: If ZKEY_AUTORUN is enabled, this module should be loaded last to ensure
+# that no keymaps are changed afterward.
+#
+
+# This parameter can be set to an empty value to disable running the guard automatically.
+: ${ZKEY_AUTORUN=on}
+
+# Path to the cache files for the main keymap.
+_ZKEY_CACHE="$ZCACHEDIR/zkeydump-$TERM"
+# List of patterns for unmonitored keys.
+_ZKEY_IGNORE=(
+  noop
+)
+
+# List/Search monitored keys.
+function zkey {
+  bindkey $1 | grep -vE ${(j:|:)_ZKEY_IGNORE} | LC_ALL=C sort
+}
+
+# Check the main keymap for changes against the last dump.
+function zkeyguard {
+  if [[ ! -r $_ZKEY_CACHE ]] {
+    [[ $TERM && $TERM != 'dumb' ]] && zkeydump
+    return
+  }
+
+  local dump diff
+  dump=${(f)"$(zkey)"}
+  diff=$(diff --color=always $_ZKEY_CACHE <(print -r $dump)) || {
+    print -P ":: %F{yellow}WARNING%f: Key bindings have changed since the last interactive run:"
+    print -r $diff
+    print ":: To accept the new keymap and suppress this warning, run 'zkeydump'"
+    return 1
+  }
+}
+
+# Accept the current keymap, dumps to the cache file.
+function zkeydump {
+  zkey >|$_ZKEY_CACHE
+}
+
+# Return if autorun is not requested.
+if [[ ! $ZKEY_AUTORUN ]] {
+  return 0
+}
+
+# Run the guard immediately or asynchronously.
+if (( ! $+functions[zinit] )) {
+  zkeyguard
+} else {
+  # Handle asynchronous printing of the output.
+  function _async_zkeyguard {
+    local out
+    out=$(zkeyguard)
+    [[ $out ]] && { print; print -r $out; zle redisplay }
+    unfunction _async_zkeyguard
+  }
+
+  # Run after all external plugins have been loaded.
+  zinit ice wait'0c' lucid nocompile \
+    id-as'guard/zkey' \
+    nocd atload'_async_zkeyguard'
+  zinit light zdharma/null
+}
---
+++ .xsh/zsh/module/zopt/@interactive.zsh
@@ -0,0 +1,84 @@
+#
+# Option monitoring module for zsh.
+#
+# NOTE: If ZOPT_AUTORUN is enabled, this module should be loaded last to ensure
+# that no options are changed afterward.
+#
+
+# This parameter can be set to an empty value to disable running the guard automatically.
+: ${ZOPT_AUTORUN=on}
+
+# Path to the cache file for zsh options.
+_ZOPT_CACHE="$ZCACHEDIR/zoptdump"
+# List of patterns for unmonitored options.
+_ZOPT_IGNORE=(
+  # shell state options
+  rcs
+  interactive
+  login
+  privileged
+  restricted
+  shinstdin
+  singlecommand
+  zle
+  # prompt options
+  promptsubst # managed by active prompt theme
+  promptcr    # powerlevel10k instant prompt support
+  promptsp    # powerlevel10k instant prompt support
+)
+
+# List options with the no's in front removed for improved comprehension,
+# i.e. `norcs off' becomes `rcs on'. It can take a list of option patterns
+# to search for via egrep.
+autoload -Uz allopt
+
+# List/Search monitored options.
+function zopt {
+  allopt $@ | grep -vE ${(j:|:)_ZOPT_IGNORE}
+}
+
+# Check the current option states for changes against the last dump.
+function zoptguard {
+  if [[ ! -r $_ZOPT_CACHE ]] {
+    zoptdump
+    return
+  }
+
+  local dump diff
+  dump=${(f)"$(zopt)"}
+  diff=$(diff --color=always $_ZOPT_CACHE <(print $dump)) || {
+    print -P ":: %F{yellow}WARNING%f: Options have changed since the last interactive run:"
+    print $diff
+    print ":: To accept the new options and suppress this warning, run 'zoptdump'"
+    return 1
+  }
+}
+
+# Accept the current option states, dumps to the cache file.
+function zoptdump {
+  zopt >|$_ZOPT_CACHE
+}
+
+# Return if autorun is not requested.
+if [[ ! $ZOPT_AUTORUN ]] {
+  return 0
+}
+
+# Run the guard immediately or asynchronously.
+if (( ! $+functions[zinit] )) {
+  zkeyguard
+} else {
+  # Handle asynchronous printing of the output.
+  function _async_zoptguard {
+    local out
+    out=$(zoptguard)
+    [[ $out ]] && { print; print $out; zle redisplay }
+    unfunction _async_zoptguard
+  }
+
+  # Run after all external plugins have been loaded.
+  zinit ice wait'0c' lucid nocompile \
+    id-as'guard/zopt' \
+    nocd atload'_async_zoptguard'
+  zinit light zdharma/null
+}
