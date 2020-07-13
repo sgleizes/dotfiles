6dd50801b50a59f993ffd3521f2cdbfea0656938
sgleizes <stephane.gleizes@gmail.com>
2020-07-13 13:49:55 +0200

    Add todotxt integration

---
+++ .xsh/zsh/module/todotxt/@interactive.zsh
@@ -0,0 +1,22 @@
+#
+# Todo.txt configuration module for zsh.
+#
+
+# Abort if requirements are not met.
+if (( ! $+commands[todo.sh] )) {
+  return 1
+}
+
+# See also:
+# https://github.com/todotxt/todo.txt-cli/blob/master/USAGE.md
+# https://github.com/todotxt/todo.txt-cli/wiki/Todo.sh-Add-on-Directory
+# Possibly interesting addons:
+# - https://github.com/rlpowell/todo-text-stuff/blob/master/ice_recur
+# - https://github.com/rlpowell/todo-text-stuff/blob/master/templated_checklists
+
+# Path to the todo.txt-cli configuration file.
+export TODOTXT_CFG_FILE="$XDG_CONFIG_HOME/todotxt/config"
+
+# Usability aliases
+alias todo='todo.sh'
+alias t='todo'
