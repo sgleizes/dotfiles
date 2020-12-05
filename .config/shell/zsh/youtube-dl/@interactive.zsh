5e4ed932d0319d54144238ea9b3803bc9c62d812
sgleizes <stephane.gleizes@gmail.com>
2020-12-05 21:37:29 +0100

    youtube-dl: Add function for ydlm

--- .config/shell/zsh/youtube-dl/@interactive.zsh
+++ .config/shell/zsh/youtube-dl/@interactive.zsh
@@ -10,9 +10,22 @@ fi
 # Usability alias.
 alias ydl='youtube-dl'
 
+# Download music to my default directory for music waiting to be added to the beets library.
+function youtube-dl-music {
+  local artist="$1"
+  local album="$2"
+  local url="$3"
+
+  ydl --extract-audio \
+    --format 'bestaudio[ext=webm]/bestaudio' \
+    --metadata-from-title '%(artist)s - %(title)s' \
+    --output "$HOME/Audios/Pending/$artist/$album/%(title)s.%(ext)s" \
+    $url ${@:4}
+}
+
 # Audio post-processing aliases.
 alias ydla='youtube-dl --extract-audio --format worstaudio' # for spoken audio
-alias ydlm='youtube-dl --extract-audio --format "bestaudio[ext=webm]/bestaudio" --metadata-from-title "%(artist)s - %(title)s"' # for music
+alias ydlm="youtube-dl-music"                               # for music
 
 # List formats & prompt for one.
 function youtube-dl-interactive {
