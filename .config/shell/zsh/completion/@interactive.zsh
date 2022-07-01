8bea59b7eb01aa355f42b26015147c0fb4b305c2
sgleizes <stephane.gleizes@gmail.com>
2022-07-01 23:31:44 +0200

    zsh: Add support for bash completions using bashcompinit
    Also add generated completions for beet and golanci-lint

--- .config/shell/zsh/completion/@interactive.zsh
+++ .config/shell/zsh/completion/@interactive.zsh
@@ -172,9 +172,24 @@ fpath+=('/usr/share/zsh/vendor-completions/')
 # Additional completion definitions
 #
 
+# Load completions which are not zsh-compatible using bashcompinit.
+# This must run after compinit.
+function _load_bash_completions {
+  autoload -Uz bashcompinit
+  bashcompinit
+
+  # Load pipx completion.
+  if (( $+commands[pipx] && $+commands[register-python-argcomplete] )); then
+    eval "$(register-python-argcomplete pipx)"
+  fi
+
+  unfunction _load_bash_completions
+}
+
 # Abort if requirements are not met, run compinit synchronously in this case.
 if (( ! $+functions[zi] )); then
   autoload -Uz compinit && compinit -d "$ZCACHEDIR/zcompdump"
+  _load_bash_completions
   return 0
 fi
 
@@ -187,4 +202,5 @@ zi light-mode wait lucid for \
   depth=1 blockf \
   atpull'zi creinstall -q .' \
   atload'zicompinit; zicdreplay' \
+  atload'_load_bash_completions' \
   @zsh-users/zsh-completions
